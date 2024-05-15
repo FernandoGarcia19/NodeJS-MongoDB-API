779921a38c985625f55094cfecbfc6abde098fc9
FernandoGarcia19 <fernando.garcia@ucb.edu.bo>
2024-05-14 21:11:54 -0400

    Add error handling

--- app.js
+++ app.js
@@ -5,6 +5,22 @@ const app = express()
 const ProductRoute = require('./Routes/Product.route')
 app.use('/products', ProductRoute)
 
+app.use((req, res, next) =>{
+    const err = new Error('Not found');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.send({
+        error: {
+            status: err.status || 500,
+            message: err.message
+        }
+    })
+})
+
 app.listen(3000, () =>
 {
     console.log('server started on port 3000')
