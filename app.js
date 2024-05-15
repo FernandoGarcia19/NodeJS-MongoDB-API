f9ba2be49629d87821b4d95d68f2b5369c329eae
FernandoGarcia19 <fernando.garcia@ucb.edu.bo>
2024-05-14 21:40:30 -0400

    MongoDB connection

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
+
+//db_access
+//jrZn6QGWhf9RwXg1
 const app = express()
+
+mongoose.connect('mongodb+srv://mycluster.gakhyll.mongodb.net/?retryWrites=true&w=majority&appName=MyCluster', {
+    dbName: 'PRODUCT_API',
+    user: 'db_access',
+    pass: 'jrZn6QGWhf9RwXg1',
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('MongoDB HAS CONNECTED....');
+})
+
 
 const ProductRoute = require('./Routes/Product.route')
 app.use('/products', ProductRoute)
